4a22f6edf6f2f20df368ca864854e25cef600adf
gallais <guillaume.allais@ens-lyon.org>
2015-08-02 18:17:22 +0100

    [ fix ] make sure to keep ALL digits

--- easy/021/Main.hs
+++ easy/021/Main.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Control.Monad
 import Data.List   as List
 import Data.Maybe
 import Data.Set    as Set
@@ -14,21 +15,23 @@ digits = fmap (read . return) . show
 unDigits :: [Int] -> Integer
 unDigits = read . concatMap show
 
-smallest :: [Int] -> Int -> [Int] -> Int -> Integer
-smallest as b bs c = unDigits $ reverse as ++ c : rearranged
-  where rearranged = reverse $ take (length bs) decreasing
-        decreasing = sortBy (flip compare) digs ++ repeat padding
+smallest :: [Int] -> Int -> [Int] -> Int -> Maybe Integer
+smallest as b bs c = do
+  guard (length digs <= length bs)
+  return $ unDigits $ reverse as ++ c : rearranged
+  where rearranged = reverse $ take (length bs) $ decreasing ++ repeat padding
+        decreasing = sortBy (flip compare) digs
         digs       = (nub $ b : bs) List.\\ (nub $ c : as)
         padding    = minimum (b : as ++ bs)
 
 next :: Integer -> Integer
 next n = finish $ Zipper.foldr cons Nothing $ Zipper.fromList $ digs
   where
-    cons as b bs = maybe (fmap (smallest as b bs) $ lookupGT b sdigs) Just
+    cons as b bs = maybe (lookupGT b sdigs >>= smallest as b bs) Just
     digs   = digits n
     sdigs  = Set.fromList digs
     min    = findMin sdigs
-    finish = fromMaybe (unDigits $ min : digs)
+    finish = fromMaybe (fromJust $ smallest [] min digs min)
 
 main :: IO ()
 main = getLine >>= print . next . read
