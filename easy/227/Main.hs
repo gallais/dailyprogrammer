4fe208121628bfc1feb24c9bdaf8199a19efb297
gallais <guillaume.allais@ens-lyon.org>
2015-08-18 12:08:28 +0100

    [ new ] find words in (reverse) alphabetical order

---
+++ easy/227/Main.hs
@@ -0,0 +1,34 @@
+module Main where
+
+import System.IO
+import System.Environment
+import Data.Char
+import Data.ByteString.Char8 as BS
+
+inAlphabeticalOrder :: ByteString -> Bool
+inAlphabeticalOrder w = sort w == w
+
+inReverseAlphabeticalOrder :: ByteString -> Bool
+inReverseAlphabeticalOrder = inAlphabeticalOrder . BS.reverse
+
+dispatch :: Handle -> Handle -> ByteString -> IO ()
+dispatch alpha ralpha w =
+  if      not (BS.all isAlpha w)       then return ()
+  else if inAlphabeticalOrder w        then BS.hPutStrLn alpha  w
+  else if inReverseAlphabeticalOrder w then BS.hPutStrLn ralpha w
+  else return ()
+
+
+main :: IO ()
+main = do
+  args   <- getArgs
+  let fp = case args of
+             []         -> "/usr/share/dict/american-english"
+             (dict : _) -> dict
+  ws     <- BS.readFile fp
+  alpha  <- openFile "alphabetical.txt"  WriteMode
+  ralpha <- openFile "ralphabetical.txt" WriteMode
+  mapM_ (dispatch alpha ralpha) $ BS.lines ws
+  hClose alpha
+  hClose ralpha
+
